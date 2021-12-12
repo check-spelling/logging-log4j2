a03788dc56f56ee5541c350b05d29016df2e582b
Josh Soref <jsoref@users.noreply.github.com>
2021-12-11 20:29:25 -0500

    spelling: wildcards
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/IfFileName.java
+++ log4j-core/src/main/java/org/apache/logging/log4j/core/appender/rolling/action/IfFileName.java
@@ -54,7 +54,7 @@ public final class IfFileName implements PathCondition {
      * Constructs a FileNameFilter filter. If both a regular expression and a glob pattern are specified the glob
      * pattern is used and the regular expression is ignored.
      *
-     * @param glob the baseDir-relative path pattern of the files to delete (may contain '*' and '?' wildcarts)
+     * @param glob the baseDir-relative path pattern of the files to delete (may contain '*' and '?' wildcards)
      * @param regex the regular expression that matches the baseDir-relative path of the file(s) to delete
      * @param nestedConditions nested conditions to evaluate if this condition accepts a path
      */
@@ -81,7 +81,7 @@ public final class IfFileName implements PathCondition {
      * regular expression} or a simplified pattern expression described under "glob" in
      * {@link FileSystem#getPathMatcher(String)}.
      *
-     * @return relative path of the file(s) to delete (may contain regular expression or wildcarts)
+     * @return relative path of the file(s) to delete (may contain regular expression or wildcards)
      */
     public String getSyntaxAndPattern() {
         return syntaxAndPattern;
@@ -126,7 +126,7 @@ public final class IfFileName implements PathCondition {
      * If both a regular expression and a glob pattern are specified the glob pattern is used and the regular expression
      * is ignored.
      *
-     * @param glob the baseDir-relative path pattern of the files to delete (may contain '*' and '?' wildcarts)
+     * @param glob the baseDir-relative path pattern of the files to delete (may contain '*' and '?' wildcards)
      * @param regex the regular expression that matches the baseDir-relative path of the file(s) to delete
      * @param nestedConditions nested conditions to evaluate if this condition accepts a path
      * @return A IfFileName condition.
